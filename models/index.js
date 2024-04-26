f60d9a9926c5df7b5022e5f78ea8c94ab5595d8e
fairleyv <fairleyvincent@gmail.com>
2024-04-26 13:27:47 -0700

    created relations in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsto(Category, {
+  foreignKey: 'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongstoMany(Tag, {
+  foreignKey: 'tag_id',
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongstoMany(Product, {
+  foreignKey: 'product_id'
+})
 module.exports = {
   Product,
   Category,

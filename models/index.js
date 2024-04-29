cabbd984dd26cb41d62313c18ff1727440ec9cbc
fairleyv <fairleyvincent@gmail.com>
2024-04-28 19:36:56 -0700

    reworked belongsToMany function for product and tag in index.js

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsto(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 })
 // Categories have many Products
@@ -13,12 +13,12 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongstoMany(Tag, {
-  foreignKey: 'tag_id',
+Product.belongsToMany(Tag, {
+  through: ProductTag,
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongstoMany(Product, {
-  foreignKey: 'product_id'
+Tag.belongsToMany(Product, {
+  through: ProductTag
 })
 module.exports = {
   Product,
